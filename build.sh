7542bbb50ed4f88e8fdd4d7cbacbd9581d8a87bc
Alpha DIALLO <moyodiallo@gmail.com>
2023-07-21 20:02:37 +0200

    install.sh, static and shared build

--- build.sh
+++ build.sh
@@ -14,3 +14,13 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DJANSSON_DIR=$JANSSON      \
 	 ../variorum/src
 make
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
+         -DENABLE_FORTRAN=OFF        \
+         -DBUILD_TESTS=OFF           \
+         -DBUILD_DOCS=OFF            \
+         -DVARIORUM_LOG=OFF          \
+         -DBUILD_SHARED_LIBS=OFF     \
+	 -DHWLOC_DIR=$HWLOC          \
+         -DJANSSON_DIR=$JANSSON      \
+	 ../variorum/src
+make
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+PREFIX=`opam var prefix`
 cd build
 make install
+cp config/variorum.pc $PREFIX/lib/pkgconfig/variorum.pc
